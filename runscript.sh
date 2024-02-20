672293fd54b632e53f7c149e623db4c96114e382
petitlunette <147147487+petitlunette@users.noreply.github.com>
2024-02-20 15:13:15 +0000

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -55,24 +55,69 @@ fi
 
 # Prompt the user for the input file location
 echo "Please enter the path to your input data directory:"
-read input_path
+read INPUT_PATH
 
 # Check if the file exists
-if [ -d "$input_path" ]; then
-    echo "Directory found at: $input_path"
+if [ -d "$INPUT_PATH" ]; then
+    echo "Directory found at: $INPUT_PATH"
 else
-    echo "Directory not found at: $input_path. Please check the path and try again."
+    echo "Directory not found at: $INPUT_PATH. Please check the path and try again."
 fi
 
 #Prompt the user for the input file type
 echo "Which input file type are you using? (pod5/fast5)"
-read file_type
+read FILE_TYPE
 
+
+# Define file paths
+VIRTUAL_ENV_PATH=$(get_config_value "Medaka" "medaka_env_path")
+DATA_OUTPUT_PATH=$(get_config_value "Data" "output_data_dir")
+PYTHON_PATH=$(which python || which python3)
 
 # Create directories for each tool output within the virtual environment
 mkdir -p $DATA_OUTPUT_PATH/dorado
-mkdir -p $DATA_OUTPUT_PATH/NanoPlot
+mkdir -p $DATA_OUTPUT_PATH/nanoplot
 mkdir -p $DATA_OUTPUT_PATH/flye
 mkdir -p $DATA_OUTPUT_PATH/meryl
 mkdir -p $DATA_OUTPUT_PATH/merqury
 mkdir -p $DATA_OUTPUT_PATH/medaka
+
+ # Ask user if they want to run Dorado for base calling
+read -p "Do you want to run Dorado for base calling? (yes/no): " run_dorado
+if [[ "$run_dorado" == "yes" ]]; then
+    echo "Running Dorado..."
+    cd $DATA_OUTPUT_PATH/dorado
+    DORADO_MODEL=$("Dorado" "default_model")
+    dorado download --model $DORADO_MODEL
+    dorado basecaller $DORADO_MODEL $INPUT_PATH/*$FILE_TYPE/ > calls.fastq.gz
+fi
+
+echo "Running NanoPlot..."
+NanoPlot --fastq calls.fastq.gz -o $DATA_OUTPUT_PATH/nanoplot
+cat NanoStats.txt #ask here if you are happy to proceed with your data? not much point in running the rest of the pipeline if your inital stats are shit
+read -p "Is the quality of the data sufficient to run the pipeline? (yes/no): " run_pipeline
+if [["$run_pipeline" == "no" ]]; then
+exit
+done
+if [["$run_pipeline" == "yes" ]]; then
+    echo "Running Flye (metaFlye)..."
+    FLYE_ITERATIONS=$(get_config_value "MetaFlye" "default_iterations")
+    FLYE_QUALITY=$(get_config_value "MetaFlye" "default_read_quality")
+    FLYE_PATH=$(get_config_value "MetaFlye" "path")
+    PYTHON_PATH=$(which python || which python3)
+    if [[ "$run_dorado" == "yes" ]]; then
+        PYTHON_PATH $flye_path --nano-hq $DATA_OUTPUT_PATH/dorado/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
+        fi
+    if [[ "$run_dorado" == "no" ]]; then
+        PYTHON_PATH $flye_path --nano-hq $INPUT_PATH/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
+        fi
+    
+    echo "Running Meryl..."
+    MERYL_KMERS=$("Meryl" "default_kmers")
+    
+    
+
+    
+
+
+
