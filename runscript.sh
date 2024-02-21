e806a83f38f0cff1f4ebd07e2a1331c580577622
petitlunette <147147487+petitlunette@users.noreply.github.com>
2024-02-21 12:19:43 +0000

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -8,13 +8,14 @@ get_config_value() {
     awk -F "=" -v section="$1" -v key="$2" '$0 ~ "\\["section"\\]" {flag=1; next} /\\[.*\\]/ {flag=0} flag && $1 ~ key {print $2; exit}' "$config_file" | tr -d ' '
 }
 
-
 # List of programs to check
-default_programs=("Dorado" "Nanoplot" "MetaFlye" "Meryl" "Merqury" "Medaka" "VALET" "Metawrap" "Kraken2")
+
+default_programs=("Dorado" "Nanoplot" "Flye" "Meryl" "Merqury" "Medaka" "VALET" "Metawrap" "Kraken2")
 optional_programs=("Racon")
 not_found_programs=()
+declare -A program_paths
 
-for program in "${default_programs[@]}"; do
+for program in "${default_programs[@]}" "${optional_programs[@]}"; do
     # Try to read program path from the configuration file first
     program_path=$(get_config_value "$program" "path")
     config_path_used=false
@@ -23,9 +24,8 @@ for program in "${default_programs[@]}"; do
         config_path_used=true
     else
         # Define standard locations for the program
-        standard_paths=("/usr/local/bin/$program" "/usr/bin/$program") # Add more paths as needed
-
-        # Check standard locations
+        standard_paths=("/usr/local/bin/$program" "/usr/bin/$program")
+         # Check standard locations
         for path in "${standard_paths[@]}"; do
             if [ -x "$path" ]; then
                 program_path="$path"
@@ -39,6 +39,8 @@ for program in "${default_programs[@]}"; do
         if [ "$config_path_used" = true ]; then
             echo "Using $program at: $program_path (from configuration file)"
         fi
+    # Store the path in the associative array
+        program_paths[$program]="$program_path"
     else
         # Program not found, add to not found list
         not_found_programs+=("$program")
@@ -87,35 +89,34 @@ read -p "Do you want to run Dorado for base calling? (yes/no): " run_dorado
 if [[ "$run_dorado" == "yes" ]]; then
     echo "Running Dorado..."
     cd $DATA_OUTPUT_PATH/dorado
-    DORADO_MODEL=$("Dorado" "default_model")
+    export PATH=${program_paths[Dorado]}:$PATH
+    DORADO_MODEL=$(get_config_value "Dorado" "default_model")
     dorado download --model $DORADO_MODEL
     dorado basecaller $DORADO_MODEL $INPUT_PATH/*$FILE_TYPE/ > calls.fastq.gz
 fi
 
 echo "Running NanoPlot..."
+export PATH=${program_paths[Nanoplot]}:$PATH
 NanoPlot --fastq calls.fastq.gz -o $DATA_OUTPUT_PATH/nanoplot
-cat NanoStats.txt #ask here if you are happy to proceed with your data? not much point in running the rest of the pipeline if your inital stats are shit
+cat $DATA_OUTPUT_PATH/nanoplot/NanoStats.txt 
 read -p "Is the quality of the data sufficient to run the pipeline? (yes/no): " run_pipeline
-if [["$run_pipeline" == "no" ]]; then
-exit
-done
-if [["$run_pipeline" == "yes" ]]; then
-    echo "Running Flye (metaFlye)..."
-    FLYE_ITERATIONS=$(get_config_value "MetaFlye" "default_iterations")
-    FLYE_QUALITY=$(get_config_value "MetaFlye" "default_read_quality")
-    FLYE_PATH=$(get_config_value "MetaFlye" "path")
-    PYTHON_PATH=$(which python || which python3)
+if [[ "$run_pipeline" == "no" ]]; then
+    exit
+fi
+if [[ "$run_pipeline" == "yes" ]]; then
+    echo "Running Flye ..."
+    FLYE_ITERATIONS=$(get_config_value "Flye" "default_iterations")
+    FLYE_QUALITY=$(get_config_value "Flye" "default_read_quality")
     if [[ "$run_dorado" == "yes" ]]; then
-        PYTHON_PATH $flye_path --nano-hq $DATA_OUTPUT_PATH/dorado/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
-        fi
-    if [[ "$run_dorado" == "no" ]]; then
-        PYTHON_PATH $flye_path --nano-hq $INPUT_PATH/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
-        fi
-    
+        ${PYTHON_PATH} ${program_paths[Flye]} --nano-hq $DATA_OUTPUT_PATH/dorado/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
+    else
+        ${PYTHON_PATH} ${program_paths[Flye]} --nano-hq $INPUT_PATH/calls.fastq.gz --out-dir $DATA_OUTPUT_PATH/flye --iterations $FLYE_ITERATIONS --meta
+    fi
     echo "Running Meryl..."
-    MERYL_KMERS=$("Meryl" "default_kmers")
-    export PATH=$MERYL_PATH:$PATH #need to specify how you are getting this from the program function
-    meryl count k=21 $DATA_OUTPUT_PATH/flye 
+    cd $DATA_OUTPUT_PATH/meryl
+    MERYL_KMERS=$(get_config_value "Meryl" "default_kmers")
+    export PATH=${program_paths[Meryl]}:$PATH
+    meryl count k=21 $DATA_OUTPUT_PATH/flye/assembly.fasta output asmDB.k21.meryl
     
     
 
