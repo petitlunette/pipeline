6849d7b34b20648e73bb93eceb4403460f9cd545
Morgan Cockrill <110744412+MCrill@users.noreply.github.com>
2024-03-07 13:00:33 +0000

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -311,4 +311,38 @@ fi
 echo "Pipeline execution completed. If you wish to rerun the pipeline or specific steps, remember to delete the '.<step_name>_done' checkpoint files from '$DATA_OUTPUT_PATH'."
 
 
-    
+    #Kraken2
+if ! check_for_checkpoint "kraken2"; then
+    DBNAME=$(get_config_value "Kraken2" "dbname")
+    database_setup_success=false
+
+    if [[ -z "$DBNAME" ]]; then
+        read -p "No Kraken2 database location found in the config. Do you have an existing Kraken2 database? (yes/no): " has_kraken_db
+        if [[ "$has_kraken_db" == "yes" ]]; then
+            read -p "Enter the location of your existing Kraken2 database: " DBNAME
+            # Consider the database setup successful since it's manually provided
+            database_setup_success=true
+        else
+            read -p "Enter a location for your new Kraken2 database: " DBNAME
+            echo "Creating and building new Kraken2 database at $DBNAME..."
+            mkdir -p "$DBNAME"
+            if kraken2-build --standard --db "$DBNAME"; then
+                database_setup_success=true
+            else
+                echo "Standard database build failed. Attempting to download prebuilt database..."
+                if wget -O "$DBNAME/k2_standard_20240112.tar.gz" "https://genome-idx.s3.amazonaws.com/kraken/k2_standard_20240112.tar.gz" && tar -xzf "$DBNAME/k2_standard_20240112.tar.gz" -C "$DBNAME" --strip-components=1; then
+                    rm "$DBNAME/k2_standard_20240112.tar.gz"
+                    database_setup_success=true
+                else
+                    echo "Failed to download the prebuilt database. Please check your internet connection or the URL and try again."
+                fi
+            fi
+        fi
+
+        if [[ "$database_setup_success" == true ]]; then
+            echo "Database $DBNAME setup complete."
+            sed -i "/^\[Kraken2\]/,/^\[/ {/^dbname=/ s|=.*|=$DBNAME|}" "$config_file"
+            echo "Kraken2 database location updated in config: $DBNAME"
+        else
+            echo "Kraken
+
